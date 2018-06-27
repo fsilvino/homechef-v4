3f7b18b7a98d88499ecefd6571a25375e36aca61
fsilvino <fsilvino@users.noreply.github.com>
2018-06-27 19:54:29 -0300

    assg

--- src/br/ufsc/ine5608/homechef/controller/ControladorPrincipal.java
+++ src/br/ufsc/ine5608/homechef/controller/ControladorPrincipal.java
@@ -2,7 +2,9 @@ package br.ufsc.ine5608.homechef.controller;
 
 import br.ufsc.ine5608.homechef.dto.DadosItemEstoque;
 import br.ufsc.ine5608.homechef.dto.DadosReceita;
+import br.ufsc.ine5608.homechef.model.Dificuldade;
 import br.ufsc.ine5608.homechef.view.MainForm;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -20,8 +22,11 @@ public class ControladorPrincipal {
     
     private static ControladorPrincipal instance;
     private MainForm telaPrincipal;
+    private ArrayList<Dificuldade> dificuldades;
 
     private ControladorPrincipal() {
+        dificuldades = ControladorDificuldade.getInstance().getAll();
+        dificuldades.add(0, new Dificuldade(0, "Todas"));
     }
 
     public static ControladorPrincipal getInstance() {
@@ -40,10 +45,28 @@ public class ControladorPrincipal {
     public void atualizaTelaPrincipal() {
         setDadosTelaPrincipal();
     }
+    
+    
     private void setDadosTelaPrincipal () {
-        List<DadosReceita> receitas = ControladorReceita.getInstance().getListaDTO();
+        atualizaItensEstoque();
+        atualizaReceitas();
+    }
+    
+    public void atualizaItensEstoque() {
         List<DadosItemEstoque> estoque = ControladorItemEstoque.getInstance().getListaDTO();
-        this.telaPrincipal.setDados(receitas, estoque);
+        telaPrincipal.setItensEstoque(estoque);
+    }
+    
+    public void atualizaReceitas() {
+        ArrayList<DadosReceita> resultado = new ArrayList<>();
+        List<DadosReceita> receitas = ControladorReceita.getInstance().getListaDTO();
+        Dificuldade dificuldade = this.dificuldades.get(telaPrincipal.getIndexDificuldade());
+        for (DadosReceita receita : receitas) {
+            if ((dificuldade.getId() == 0 || receita.dificuldade.getId() == dificuldade.getId())) {
+                resultado.add(receita);
+            }
+        }
+        telaPrincipal.setReceitas(resultado);
     }
     
 }
